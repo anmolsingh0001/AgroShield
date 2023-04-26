fe129ad1af194c2b9fb7f3f7ec26f06f3d635e49
anmolsingh0001 <anmol.singh@kalvium.community>
2023-04-26 17:16:02 +0530

    change cors

--- backend/server.js
+++ backend/server.js
@@ -12,7 +12,10 @@ const url = require('url');
 
 
 const app = express();
-app.use(cors())
+app.use(  cors({
+  origin: ["http://localhost:3000", "https://agroshield.netlify.app"],
+})
+)
 app.use(express.json());
 app.use(express.json({ limit: "50mb", extended: true }));
 app.use(express.urlencoded({ extended: false, limit: "50mb" }));
